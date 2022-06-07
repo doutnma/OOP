9a5dc188e356e5226b898e8b08bba47adb6938e4
doutnma20 <doutnma20@sps-prosek.cz>
2022-06-07 15:59:41 +0200

    Dekorátor

--- set, get parametry.py
+++ set, get parametry.py
@@ -1,4 +1,5 @@
 class Person:
+
     def __init__(self):
         self.__money = 0
 
@@ -13,8 +14,10 @@ p1.set_money(100)
 p1.get_money()
 
 class Person:
+
     def __init__(self):
         self.__money = 0
+        self.birth = 2004
 
     @property
     def money(self):
@@ -22,8 +25,33 @@ class Person:
 
     @money.setter
     def money(self, amount):
-        self.__money += amount
+        if amount > 0:
+            self.__money += amount
+        else:
+            print("error")
+
+    @property
+    def name(self):
+        return self.__money
+
+    @name.setter
+    def name(self, amount):
+        if amount > 0:
+            self.__money += amount
+        else:
+            print("too short")
+
+    def add_money_to_account(self, amount, eur, date):
+        self.money += amount
+        self.eur = eur
+        self.date = date
+
+    @property
+    def age(self):
+        return 2022 - self.birth
 
 p2 = Person()
 p2.money = 100
-p2.money
+p2.money = -10
+print(p2.money)
+print(p2.age)
